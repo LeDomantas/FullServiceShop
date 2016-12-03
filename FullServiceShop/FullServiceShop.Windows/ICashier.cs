11a1bc4370db19f7e99456a8e56bd58c3af04e91
leDomantas <domantas.lekavicius@gmail.com>
2016-12-03 12:22:48 +0200

    Added ICashier and IGoodsCollector
    Two interfaces for shop workers

---
+++ FullServiceShop/FullServiceShop.Windows/ICashier.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullServiceShop
+{
+    interface ICashier
+    {
+        //void PrintReceipt();
+        //void TakeCash();
+    }
+}
